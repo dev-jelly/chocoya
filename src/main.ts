36fea2c9d2c7bd358a25e7c985e302d3c52a33b8
jelly <chm073@gmail.com>
2024-02-28 22:49:56 +0900

    프로젝트 정보들 업데이트

--- src/main.ts
+++ src/main.ts
@@ -1,64 +1,110 @@
 import "./style.css";
 import { setupCounter } from "./counter.ts";
 
+type Project = {
+  title: string;
+  description: string;
+  link: string;
+};
+
+const chocoyaProjects: Project[] = [
+  {
+    title: "초코야! 이것 좀 해라!",
+    description: "로컬 기반 투두앱으로, 런칭 속도가 매우 빠른게 특징입니다.",
+    link: "https://github.com/dev-jelly/tauri-ww-todo",
+  },
+  {
+    title: "초코야! 스플랫 플레이트",
+    description:
+      "스플래툰의 요소 중 하나인 플레이트를 편집 및 제작하기 쉽게 만들어주는 사이트입니다. 원본은 <a href='https://seymourschlong.github.io/splashtags/'>seymourschlong의 splashtag</a>이며, 한번 <a href='https://dev-jelly.github.io/splashtags/'>포크해서 인쇄용으로 만든 프로젝트</a>를 다시 사용성을 개선해 만든 프로젝트입니다.",
+    link: "https://dev-jelly.github.io/splat-plate/",
+  },
+  {
+    title: "초코야! 연어한판?!",
+    description:
+      "스플래툰 연어런 공략을 위해 만든 사이트이며, 연어런 로테이션을 확인하고 로테이션별 무기 공략을 확인할 수 있습니다.",
+    link: "https://dev-jelly.github.io/today-salmonrun/",
+  },
+  {
+    title: "초코야! 스플랫 사전",
+    description:
+      "스플래툰 용어 중 번역하기 어려운 단어들을 게임내 데이터를 검색해서 보여주는 사이트 입니다. 사전을 서빙하는 코드는 보안을 위해 비공개로 해두었습니다.",
+    link: "https://github.com/dev-jelly/splat-dictionary/",
+  },
+  {
+    title: "초코야! 스플랫 번역",
+    description:
+      "Gen AI 기반의 스플래툰 번역기 입니다. 스플래툰 관련 문장을 입력하면 한국어로 번역할 수 있게 끔 하는 게 목표입니다.(개발중)",
+    link: "#",
+  },
+  {
+    title: "초코야! 주기 관리",
+    description:
+      "일상적으로 갈아줘야할 부품이나 주기에 맞춰 해야하는 것들을 관리하기 위한 프로젝트입니다. (기획단계)",
+    link: "#",
+  },
+];
+
+const projectList = chocoyaProjects.map((project) => {
+  if (project.link === "#")
+    return `
+  <li class="flex flex-col gap-x-3">
+  <span class="font-semibold text-lg text-gray-900">${project.title}</span>
+    <span> ${project.description}</span>
+  </li>
+  `;
+  return `
+  <li class="flex flex-col gap-x-3">    
+  <a class="font-semibold text-lg text-gray-900" href="${project.link}" target="_blank">${project.title}</a>
+    <span> ${project.description}</span>
+  </li>
+  `;
+});
+
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div class="bg-white px-6 py-32 lg:px-8">
   <div class="mx-auto max-w-3xl text-base leading-7 text-gray-700">
     <p class="text-base font-semibold leading-7 text-indigo-600">Introducing</p>
-    <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">초코야 프로젝트에 오신 걸 환영합니다.</h1>
-    <p class="mt-6 text-xl leading-8">
-        초코야 프로젝트는 젤리와 초코를 중심으로 매번 본인 필요한 거 만들어서 사용하는 프로젝트입니다.    
+    <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl break-keep">초코야 프로젝트에 오신 걸 환영합니다.</h1>
+   
+    <p class="mt-6 text-xl leading-8 mx-24 break-keep">
+        초코야 프로젝트는 젤리와 초코를 중심으로 매번 본인들 필요한 거 만들어서 사용하는 프로젝트입니다.    
     </p>
-    <div class="mt-10 max-w-2xl">
+
+<!--  Profiles  -->    
+    <div class="flex flex-col md:flex-row justify-center items-center">
+      <div class="mt-10 sm:w-1/2">
+        <div class="flex my-2 w-full justify-center items-center">
+          <div class="w-24 h-24 rounded-full">
+            <img src="/jelly.png" alt="젤리" class="w-24 h-24 rounded-full">
+          </div>
+        </div>
+        <a href="https://github.com/dev-jelly" class="text-xl font-semibold text-gray-900">젤리</a>
+        <p class="mt-2 text-base leading-7 text-gray-600">
+          젤리는 개발자로 기술에 관심이 많습니다.
+        </p>
+      </div>
+      <div class="mt-10 sm:w-1/2">
+        <div class="flex my-2 w-full justify-center items-center">
+          <div class="w-24 h-24 rounded-full">
+            <img src="/choco.png" alt="초코" class="w-24 h-24 rounded-full">
+          </div>
+        </div>
+        <a href="https://twitter.com/octoling_choco" class="text-xl font-semibold text-gray-900">초코</a>
+        <p class="mt-2 text-base leading-7 text-gray-600">
+          초코는 기획자로 여러가지 잔소리가 많습니다!
+        </p>
+      </div>
+    </div>
+    
+    <div class="mt-10 flex flex-col justify-center items-center w-full">
       <p>
       초코야 프로젝트에서 진행한 프로젝트들
         </p>
-      <ul role="list" class="mt-8 max-w-xl space-y-8 text-gray-600">
-        <li class="flex gap-x-3">    
-          <span><a class="font-semibold text-gray-900" href="https://github.com/dev-jelly/tauri-ww-todo">초코야! 이것 좀 해라!</a>로컬 기반 투두앱으로, 기본적인 투두리스트 기능들을 가지고 있고, 데이터는 전부 로컬스토리지에 저장하고 있습니다. Svelte와 tauri 를 이용하여 작성되었고 런칭 시 시간이 매우 짧은 게 특징입니다.</span>
-        </li>
-        <li class="flex gap-x-3">
-      
-          <span>
-          <strong class="font-semibold text-gray-900">
-          초코야! 연어한판?
-          </strong> Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.</span>
-        </li>
-        <li class="flex gap-x-3">
-          <span><strong class="font-semibold text-gray-900">초코야! 스플랫 플레이트!</strong> Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</span>
-        </li>
-      </ul>
-      <p class="mt-8">Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices hac adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.</p>
-      <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">From beginner to expert in 3 hours</h2>
-      <p class="mt-6">Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis diam.</p>
-      <figure class="mt-10 border-l border-indigo-600 pl-9">
-        <blockquote class="font-semibold text-gray-900">
-          <p>“Vel ultricies morbi odio facilisi ultrices accumsan donec lacus purus. Lectus nibh ullamcorper ac dictum justo in euismod. Risus aenean ut elit massa. In amet aliquet eget cras. Sem volutpat enim tristique.”</p>
-        </blockquote>
-        <figcaption class="mt-6 flex gap-x-4">
-          <img class="h-6 w-6 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-          <div class="text-sm leading-6"><strong class="font-semibold text-gray-900">Maria Hill</strong> – Marketing Manager</div>
-        </figcaption>
-      </figure>
-      <p class="mt-10">Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit.</p>
+      <ul role="list" class="mt-8 max-w-xl space-y-8 text-gray-600 break-keep">
+        ${projectList.join("")}
+      </ul>  
     </div>
-    <figure class="mt-16">
-      <img class="aspect-video rounded-xl bg-gray-50 object-cover" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3" alt="">
-      <figcaption class="mt-4 flex gap-x-2 text-sm leading-6 text-gray-500">
-        <svg class="mt-0.5 h-5 w-5 flex-none text-gray-300" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-          <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a.75.75 0 000 1.5h.253a.25.25 0 01.244.304l-.459 2.066A1.75 1.75 0 0010.747 15H11a.75.75 0 000-1.5h-.253a.25.25 0 01-.244-.304l.459-2.066A1.75 1.75 0 009.253 9H9z" clip-rule="evenodd" />
-        </svg>
-        Faucibus commodo massa rhoncus, volutpat.
-      </figcaption>
-    </figure>
-    <div class="mt-16 max-w-2xl">
-      <h2 class="text-2xl font-bold tracking-tight text-gray-900">Everything you need to get up and running</h2>
-      <p class="mt-6">Purus morbi dignissim senectus mattis adipiscing. Amet, massa quam varius orci dapibus volutpat cras. In amet eu ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut viverra ridiculus non molestie. Gravida quis fringilla amet eget dui tempor dignissim. Facilisis auctor venenatis varius nunc, congue erat ac. Cras fermentum convallis quam.</p>
-      <p class="mt-8">Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit.</p>
-    </div>
-  </div>
-</div>
-
 `;
 
 setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
