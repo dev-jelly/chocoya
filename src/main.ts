17aa001d0e255ef53a2196d9ae4e614ae692c6f3
jelly <chm073@gmail.com>
2024-03-16 23:05:04 +0900

    스타일 변경

--- src/main.ts
+++ src/main.ts
@@ -62,7 +62,7 @@ const projectList = chocoyaProjects.map((project) => {
 });
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div class="bg-white px-4 sm:px-6 py-32 lg:px-8">
+  <div class="bg-white px-2 sm:px-6 py-32 lg:px-8 rounded-sm">
   <div class="mx-auto max-w-3xl text-base leading-7 text-gray-700">
     <p class="text-base font-semibold leading-7 text-indigo-600">Introducing</p>
     <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl break-keep">초코야 프로젝트에 오신 걸 환영합니다.</h1>
